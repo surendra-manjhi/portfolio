61ab71d02ed409fc6b67c7c71334c30d4add0077
Surendra <manjhisurendra08@gmail.com>
2024-02-02 00:04:56 +0530

    Added home section and some fixes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// TECH STACKS
+// STACKS
 
 const techIconsContainer = document.querySelector("#tech-icons-container");
 
@@ -19,10 +19,12 @@ const techIcons = [
 
 techIcons.forEach((icon) => {
 	let imgCtn = document.createElement("div");
-	imgCtn.className = "w-12 rounded overflow-hidden";
+	imgCtn.className = "w-12";
 
 	let img = document.createElement("img");
 	img.src = `./assets/tech-icons/${icon}.png`;
+	img.alt = `${icon}`;
+	img.className = "rounded";
 
 	imgCtn.append(img);
 	techIconsContainer.append(imgCtn);
@@ -30,52 +32,66 @@ techIcons.forEach((icon) => {
 
 // PROJECTS
 
-const projectsContainer = document.querySelector("#projects-container");
+const projectCardContainer = document.querySelector("#project-card-container");
 
-const projectsData = [
+const projectData = [
 	{
 		img: "javascript",
 		name: "JavaScript",
 		live: "https://facebook.com",
-		desc: "",
+		desc: `
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore eaque odio temporibus veritatis repudiandae rem sit omnis, explicabo officiis est?
+			`,
 		tags: ["html", "css"],
 	},
 	{
 		img: "mongodb",
 		name: "MongoDB",
 		live: "https://google.com",
-		desc: "",
+		desc: `
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore eaque odio temporibus veritatis repudiandae rem sit omnis, explicabo officiis est?
+			`,
 		tags: ["mongodb", "node"],
 	},
 	{
 		img: "react",
 		name: "React",
 		live: "https://instagram.com",
-		desc: "",
+		desc: `
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore eaque odio temporibus veritatis repudiandae rem sit omnis, explicabo officiis est?
+			`,
 		tags: ["react", "tailwind"],
 	},
 ];
 
-projectsData.forEach((project, index) => {
-	projectsContainer.innerHTML += `
+projectData.forEach((project, index) => {
+	projectCardContainer.innerHTML += `
 		<div id="p-${index}" class="border-2 rounded overflow-hidden">
 			<div
 				class="p-img-wrapper aspect-4/3 overflow-hidden"
 			>
 				<img
 					src="./assets/tech-icons/${project.img}.png"
-					alt="p-img"
+					alt="${project.name}"
 					class="w-full"
 				/>
 			</div>
 
 			<div class="p-content p-2">
 				<div class="flex justify-between">
-					<div class="p-name text-xl font-medium">
+					<div class="p-name flex items-end text-xl font-medium">
 						${project.name}
+						<span class="expand cursor-pointer material-symbols-outlined">
+							expand_more
+						</span>
 					</div>
 
 					<div class="p-redirects">
+						<a href="${project.live}" target="_blank">
+							<span class="material-symbols-outlined">
+								deployed_code
+							</span>
+						</a>
 						<a href="${project.live}" target="_blank">
 							<span class="material-symbols-outlined">
 								open_in_new
@@ -84,24 +100,43 @@ projectsData.forEach((project, index) => {
 					</div>
 				</div>
 
-				<!-- <p class="p-desc text-gray-400 font-thin">
+				<p class="p-desc hidden text-gray-400 font-thin">
 					${project.desc}
-				</p> -->
+				</p>
 
-				<ul class="p-tags mt-1 flex gap-2"></ul>
+				<ul class="p-tags mt-1 flex gap-1"></ul>
 			</div>
-		</div>	
+		</div>
 	`;
 
-	let tagsCtn = document.querySelector(
-		`#projects-container #p-${index} .p-tags`
+	let tagCtn = document.querySelector(
+		`#project-card-container #p-${index} .p-tags`
 	);
 
 	project.tags.forEach((tag) => {
-		tagsCtn.innerHTML += `
+		tagCtn.innerHTML += `
 			<li class="px-2 bg-white text-black rounded">
 				${tag}
 			</li>
 		`;
+	});
+});
+
+let expandMore = Array.from(document.querySelectorAll(`.expand`));
+
+expandMore.forEach((expand) => {
+	expand.addEventListener("click", function (e) {
+		let pDesc =
+			e.target.parentElement.parentElement.parentElement.querySelector(
+				"p"
+			);
+
+		let isHidden = pDesc.classList.contains("hidden");
+
+		if (isHidden) {
+			pDesc.classList.remove("hidden");
+		} else {
+			pDesc.classList.add("hidden");
+		}
 	});
 });
